8442e1e604af6c47bfad436c7469eea4a7953a9a
zaitianhk <166913689+zaitianhk@users.noreply.github.com>
2024-05-26 14:39:25 +0400

    @zaitianhk

--- New Text Document.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call _internal\venv\Scripts\activate.bat
-
-python _internal\run.py --execution-provider cuda
--- _internal/modules/metadata.py
+++
@@ -1,3 +0,0 @@
-name = 'DeepFakeLive By ZAI TIAN'
-version = '2.0.3'
-edition = 'Standard'
